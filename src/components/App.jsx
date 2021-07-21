d4e93864aa1aed41f349d40daf0e8969bebaa0e4
Sahil Tah <sahil.tah12@gmail.com>
2021-07-21 20:16:58 +0530

    Loading multiple notes from notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,19 @@ import '../styles.css';
 import Heading from './Heading.jsx';
 import Footer from './Footer.jsx';
 import Note from './Note.jsx';
+import notes from '../notes';
 
 function App() {
     return (
         <div>
             <Heading />
-            <Note />
+            {notes.map(note =>
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            )}
             <Footer />
         </div>
     );
