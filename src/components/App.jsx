49d9f82eed17825c71040dcab6ad4b1cb79cee14
Sahil Tah <sahil.tah12@gmail.com>
2021-08-05 14:09:37 +0530

    adding add and delete note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,41 @@
-import React from 'react';
-import '../styles.css';
-import Heading from './Heading.jsx';
-import Footer from './Footer.jsx';
-import Note from './Note.jsx';
-import notes from '../notes';
+import React, { useState } from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div>
             <Heading />
-            {notes.map(note =>
-                <Note
-                    key={note.key}
-                    title={note.title}
-                    content={note.content}
-                />
-            )}
+            <CreateArea onAdd={addNote} />
+            {notes.map((noteItem, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                    />
+                );
+            })}
             <Footer />
         </div>
     );
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Heading() {
     return <header>
-        <h1>Keeper App</h1>
+        <h1>Keeper App 📝</h1>
     </header>;
 }
 
